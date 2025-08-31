f89a5779623eddba9522cccab0d2abf4dc9c37e1
utsavdulal <utsavdulal6@gmail.com>
2025-08-31 08:00:48 +0545

    addition of 1 to 100 using  for loop

---
+++ Day-5/range.py
@@ -0,0 +1,6 @@
+# ########### write a program to print from 1 to 100 and add those numbers
+# python and range
+total_sum = 0
+for number in range(0, 101):
+    total_sum += number
+print(total_sum)
