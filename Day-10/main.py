3e0c4ab97c9df8f4249dfd3ab2443d7505590276
utsavdulal <utsavdulal6@gmail.com>
2025-09-05 23:13:33 +0545

    functions with output

--- Day-10/main.py
+++ Day-10/main.py
@@ -1,12 +1,14 @@
 # Functions with outputs
 def format_name(f_name, l_name):
-    """
+    """using docstring:
     docstring :
     i am utsav dulal and i am from itahari sunsari i am the recent graduate of sushma koirala memorial engineering college
     """
     if f_name == "" or l_name == "":
         return "Please provide a valid input !"
-    formatted_f_naems = f_name.title()
+    formatted_f_naems = (
+        f_name.title()
+    )  # .title() sunction is used to format any input into formatted like input :UtsaV , Output will be : Utsav
     formatted_l_name = l_name.title()
     return f"{formatted_f_naems} {formatted_l_name}"
 
