c40bb454c84e66bad391e053c7ad49ec9518122c
utsavdulal <utsavdulal6@gmail.com>
2025-09-05 23:13:09 +0545

    functions with ouptup

---
+++ Day-10/main.py
@@ -0,0 +1,17 @@
+# Functions with outputs
+def format_name(f_name, l_name):
+    """
+    docstring :
+    i am utsav dulal and i am from itahari sunsari i am the recent graduate of sushma koirala memorial engineering college
+    """
+    if f_name == "" or l_name == "":
+        return "Please provide a valid input !"
+    formatted_f_naems = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{formatted_f_naems} {formatted_l_name}"
+
+
+print(format_name(input("What is your first name?"), input("what is your last name?")))
+
+# name = input("Enter your name : ").title()
+# print(name)
